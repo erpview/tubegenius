76f3f79a732b6a0c43a47560b48f1a9fbc3f5ee5
erpview <p.jaworski@erp-view.pl>
2025-12-03 21:55:09 +0100

    Simplify Railway deployment - use start.sh script

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e
+
+echo "Installing yt-dlp..."
+pip install --upgrade yt-dlp
+
+echo "Installing server dependencies..."
 cd server
 npm install
+
+echo "Starting server..."
 node server.js
